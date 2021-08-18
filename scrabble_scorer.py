c28404bd570c5f90ec9e896f0525b8609e53c31a
Mike <mike.sierra.7950@gmail.com>
2021-08-18 13:00:22 -0400

    FINAL

--- scrabble_scorer.py
+++ scrabble_scorer.py
@@ -21,6 +21,15 @@ def old_scrabble_scorer(word):
 				letterPoints += 'Points for {char}: {point_value}\n'.format(char = char, point_value = point_value)
 	return letterPoints
 
+def transform(dict):
+	new_dict = {}
+	new_dict[" "] = 0
+	for key in dict:
+		for char in (dict[key]):
+			new_dict[char.lower()] = key
+	return new_dict
+
+new_point_structure = transform(OLD_POINT_STRUCTURE)
 # your job is to finish writing these functions and variables that we've named
 # don't change the names or your program won't work as expected.
 
@@ -31,7 +40,7 @@ def initial_prompt():
 		print("Please enter a valid word.")
 		user_word = input("Please enter a word to be scored: ")
 	for i in range(len(user_word)):
-		while user_word[i] not in string.ascii_letters:
+		while user_word[i] not in new_point_structure.keys():
 			print("Please enter a valid word.")
 			user_word = input("Please enter a word to be scored: ")
 	return user_word
@@ -89,15 +98,6 @@ def scorer_prompt(word):
 	user_choice = int(user_choice)
 	print(f"Score for '{word}': {scoring_algorithms[user_choice]['scoring_function'](word)}")
 
-def transform(dict):
-	new_dict = {}
-	new_dict[" "] = 0
-	for key in dict:
-		for char in (dict[key]):
-			new_dict[char.lower()] = key
-	return new_dict
-
-new_point_structure = transform(OLD_POINT_STRUCTURE)
 
 def run_program():
 	word = initial_prompt()
